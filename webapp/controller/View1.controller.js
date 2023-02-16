2d1a0a89e3f2d1981ce70316bdd1ea679b9b2caa
achrafmejd <achraf.mejd@ensam-casa.ma>
2023-02-16 16:43:29 +0100

    Data displayed

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
+    // "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
     function (Controller) {
         "use strict";
-
         return Controller.extend("appartments.controller.View1", {
             onInit: function () {
                 this.onReadAll();
@@ -17,13 +17,16 @@ sap.ui.define([
                 oModel.read("/APPARTMENTSHeadersSet", {
                     success: function(oData) {
                         console.log(oData);
-                        const jModel = sap.ui.model.json.JSONModel(oData);
-                        that.getView().ById("IdProducts").setModel(jModel);
+                        
+                        const jModel = new sap.ui.model.json.JSONModel(oData);      
+                        
+                        that.getView("idProducts").setModel(jModel)
                     },
                     error: function(oError) {
                         console.log(oError);
                     }
                 });
+            
             }
         });
     });
