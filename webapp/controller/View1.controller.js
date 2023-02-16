a0cf58073d7dcae5c9da389b94129ad20b8b2aa5
achrafmejd <achraf.mejd@ensam-casa.ma>
2023-02-16 14:55:24 +0100

    101

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("appartments.controller.View1", {
+            onInit: function () {
+
+            }
+        });
+    });
