932625e62a22b70123154a5d9a205f297cf57ff9
achrafmejd <achraf.mejd@ensam-casa.ma>
2023-02-19 12:11:01 +0100

    Delete function done

--- webapp/controller/ObjectPage.controller.js
+++ webapp/controller/ObjectPage.controller.js
@@ -5,27 +5,13 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("appartments.controller.ObjectPage", {
-
         onInit: function () {
-            // set explored app's demo model on this sample
-            // var oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/products.json"));
-            // this.getView().setModel(oModel);
-            // this.byId("idPage").bindElement("/ProductCollection/0");
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.getRoute("appartmentsDetails").attachPatternMatched(this._onItemMatched, this);
         },
         _onItemMatched: function (oEvent) {
             var that = this
             var sItemId = oEvent.getParameter("arguments").id;
-            console.log(sItemId);
-            // var oModel = this.getView().getModel("myModel");
-            // var oItem = oModel.getProperty("/items/" + sItemId);
-
-            // // Set the item data on the view
-            // this.getView().bindElement({
-            //     path: "/items/" + sItemId,
-            //     model: "myModel"
-            // });
             const oModel = this.getOwnerComponent().getModel();
             oModel.read(`/APPARTMENTSHeadersSet('${sItemId}')`, {
                 success: function (oData) {
@@ -46,6 +32,41 @@ sap.ui.define([
                 price = parseInt(price, 10);
             }
             return new sap.ui.model.type.Currency().formatValue([price, currency], "string");
+        },
+        onDelete: function(oEvent){
+            // Delete Item from tables
+            var oObjectHeader = this.byId("_IDGenObjectHeader1");
+            const oModel = this.getOwnerComponent().getModel();
+            var oObject = oObjectHeader.getBindingContext().getObject();
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            
+            // oRouter.navTo("RouteView1");
+
+            sap.m.MessageBox.show(
+                "Are you sure you want to delete this item?",
+                {
+                  icon: sap.m.MessageBox.Icon.WARNING,
+                  title: "Delete",
+                  actions: [sap.m.MessageBox.Action.OK, sap.m.MessageBox.Action.CANCEL],
+                  onClose: function(oAction) {
+                    if (oAction === sap.m.MessageBox.Action.OK) {
+                      // Delete item logic here
+                      //   that.deleteItem();
+                      console.log(oObject);
+                      oModel.remove(`/APPARTMENTSHeadersSet('${oObject.Identifiant}')`,{
+                        success: function(){
+                            oRouter.navTo("RouteView1");
+                        }
+                      })
+                    }
+                  }
+                })            
+        },
+        _deleteItem: function(){
+            var oObjectHeader = this.byId("_IDGenObjectHeader1");
+            var oObject = oObjectHeader.getBindingContext().getObject();
+
+            oModel.dele
         }
         
     });
